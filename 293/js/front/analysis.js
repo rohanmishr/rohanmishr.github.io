179971399c4022e2781e7cdee46ff277a3b315d7
rohanmishr <rouangang@gmail.com>
2023-04-07 21:21:06 -0400

    team listing

--- 293/js/front/analysis.js
+++ 293/js/front/analysis.js
@@ -3,15 +3,20 @@ function renderAnalyzers(DATASET){
     $("#analyze-manual")[0].innerHTML = "";
     for(var i = 0; i < teams.length; i++){
         $("#analyze-manual")[0].innerHTML += `
-            <div class="analyzer-team"><p>${teams[i]}<p></div>
+            <div id="${teams[i]}" class="analyzer-team">
+                <p>${teams[i]}<p>
+                <div class="analyze-manual-buttons-container">
+                    <button class="upbtn" onclick="moveUp('${teams[i]}')">&#9654;</button>
+                    <button class="downbtn" onclick="moveDown('${teams[i]}')">&#9654;</button>
+                </div>
+            </div>
         `;
     }
-
     const container = document.getElementById('analyze-manual');
     const items = container.querySelectorAll('.analyzer-team');
     let draggingItem = null;
 
-    items.forEach((item)=> {
+    /*items.forEach((item)=> {
         item.addEventListener('mousedown', (e) => {
             draggingItem = item;
             container.insertBefore(document.createElement('div'), item);
@@ -30,5 +35,25 @@ function renderAnalyzers(DATASET){
             }
             draggingItem = null;
         }
-    });
+    });*/
+}
+
+function moveDown(id){
+    var el = document.getElementById(id);
+    var next = el.nextElementSibling;
+    if(next){
+        el.parentNode.insertBefore(next, el);
+        el.classList.add('dragging');
+        setTimeout(function(){el.classList.remove('dragging')},1000);
+    }
+}
+
+function moveUp(id){
+    var el = document.getElementById(id);
+    var prev = el.previousElementSibling;
+    if(prev){
+        el.parentNode.insertBefore(el, prev);
+        el.classList.add('dragging');
+        setTimeout(function(){el.classList.remove('dragging')},1000);
+    }
 }
