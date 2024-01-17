5da500bc8f61b1062c77d09bb4594b7d4d9dfea6
rohanmishra <104006850+rohanmishr@users.noreply.github.com>
2024-01-17 14:00:36 -0500

    thing

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,6 +1,3 @@
-function scrollNext() {
-    var height = window.innerHeight;
-    var scroll = window.scrollY;
-    var page = Math.floor(scroll / height);
-    window.scrollTo(0, (page + 1) * height);
-}
+document.querySelector("#header-title").addEventListener("click", () => {
+    window.location.href = "./index.html";
+})
